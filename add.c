2ff7c5d05a1a07889b5291ff736e7c7451af2405
miraayat-create <miraayat2025@gmail.com>
2025-05-18 14:04:53 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int a; int b;
+    // arithmetic operators
+    printf("ENTER A: ");
+    scanf("%d", &a);
+
+    printf("ENTER B: ");
+    scanf("%d", &b);
+    printf("The sum of %d and %d numbers is %d",a,b,a+b);
+    
+}
